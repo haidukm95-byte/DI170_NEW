e7a0f0a6bde0d0d07d4c858915c0b9d8f47fcf4a
Marko Haiduk <haidukm95@gmail.com>
2026-04-22 19:09:25 +0300

    W16D2DC fix

--- Week_16/Day2/DailyChallenge/carousel/vite.config.js
+++ Week_16/Day2/DailyChallenge/carousel/vite.config.js
@@ -4,7 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-  },
+  base: "https://github.com/haidukm95-byte/DI170_NEW.git",
 });
