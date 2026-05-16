b2129db82e9d7af5aa70cfd3b3dad8d417c6f50b
Marko Haiduk <haidukm95@gmail.com>
2026-05-16 22:25:22 +0300

    Mini project update

--- Week_19/Mini-project/src/App.jsx
+++ Week_19/Mini-project/src/App.jsx
@@ -5,6 +5,10 @@ import BooksList from './features/booksSelect/BookList';
 import SelectFantasy from './features/booksSelect/Fantasy';
 import SelectHorror from './features/booksSelect/Horror';
 import SelectSciFi from './features/booksSelect/SciFi';
+import './features/library.png';
+import './features/horror.png';
+import './features/fantasy.png';
+import './features/library.png';
 
 
 function App() {
@@ -18,22 +22,22 @@ function App() {
     <div id='main-wrapper'>
       <div id='books-list'>
         <h2><button onClick={() => setShowBooks(prev => !prev)}>{showBooks ? 'Hide' : 'View the entire booklist'}</button></h2>
-        {showBooks && <BooksList />}
+        <div className='books-names'>{showBooks && <BooksList />}</div>
       </div>
 
       <div id='books-list-scifi'>
         <h2><button onClick={() => setShowScifiBooks(prev => !prev)}>{showScifiBooks ? 'Hide' : 'Science Fiction'}</button></h2>
-        {showScifiBooks && <SelectSciFi />}
+        <div className='books-names'>{showScifiBooks && <SelectSciFi />}</div>
       </div>
 
       <div id='books-list-fantasy'>
         <h2><button onClick={() => setShowFantasyBooks(prev => !prev)}>{showFantasyBooks ? 'Hide' : 'Fantasy'}</button></h2>
-        {showFantasyBooks && <SelectFantasy />}
+        <div className='books-names'>{showFantasyBooks && <SelectFantasy />}</div>
       </div>
 
       <div id='books-list-horror'>
         <h2><button onClick={() => setShowHorrorBooks(prev => !prev)}>{showHorrorBooks ? 'Hide' : 'Horror'}</button></h2>
-        {showHorrorBooks && <SelectHorror />}
+        <div className='books-names'>{showHorrorBooks && <SelectHorror />}</div>
       </div>
 
 
